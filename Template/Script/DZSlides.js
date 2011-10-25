63d78b2a24114a2c94ac3036eaac2cf79ab99b41
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-10-25 11:53:30 +0200

    Move helper to the top of the file.

--- Template/Script/DZSlides.js
+++ Template/Script/DZSlides.js
@@ -1,3 +1,44 @@
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function (oThis) {
+
+    // closest thing possible to the ECMAScript 5 internal IsCallable
+    // function 
+    if (typeof this !== "function")
+    throw new TypeError(
+      "Function.prototype.bind - what is trying to be fBound is not callable"
+    );
+
+    var aArgs = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP = function () {},
+        fBound = function () {
+          return fToBind.apply( this instanceof fNOP ? this : oThis || window,
+                 aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+
+    return fBound;
+  };
+}
+
+
+
+var $ = (HTMLElement.prototype.$ = function(aQuery) {
+  return this.querySelector(aQuery);
+}).bind(document);
+
+var $$ = (HTMLElement.prototype.$$ = function(aQuery) {
+  return this.querySelectorAll(aQuery);
+}).bind(document);
+
+NodeList.prototype.forEach = function(fun) {
+  if (typeof fun !== "function") throw new TypeError();
+  for (var i = 0; i < this.length; i++) {
+    fun.call(this, this[i]);
+  }
+}
 var Dz = {
   remoteWindows: [],
   idx: -1,
@@ -291,46 +332,3 @@ window.onkeydown = Dz.onkeydown.bind(Dz);
 window.onresize = Dz.onresize.bind(Dz);
 window.onhashchange = Dz.onhashchange.bind(Dz);
 window.onmessage = Dz.onmessage.bind(Dz);
-
-
-
-
-if (!Function.prototype.bind) {
-  Function.prototype.bind = function (oThis) {
-
-    // closest thing possible to the ECMAScript 5 internal IsCallable
-    // function 
-    if (typeof this !== "function")
-    throw new TypeError(
-      "Function.prototype.bind - what is trying to be fBound is not callable"
-    );
-
-    var aArgs = Array.prototype.slice.call(arguments, 1),
-        fToBind = this,
-        fNOP = function () {},
-        fBound = function () {
-          return fToBind.apply( this instanceof fNOP ? this : oThis || window,
-                 aArgs.concat(Array.prototype.slice.call(arguments)));
-        };
-
-    fNOP.prototype = this.prototype;
-    fBound.prototype = new fNOP();
-
-    return fBound;
-  };
-}
-
-var $ = (HTMLElement.prototype.$ = function(aQuery) {
-  return this.querySelector(aQuery);
-}).bind(document);
-
-var $$ = (HTMLElement.prototype.$$ = function(aQuery) {
-  return this.querySelectorAll(aQuery);
-}).bind(document);
-
-NodeList.prototype.forEach = function(fun) {
-  if (typeof fun !== "function") throw new TypeError();
-  for (var i = 0; i < this.length; i++) {
-    fun.call(this, this[i]);
-  }
-}
